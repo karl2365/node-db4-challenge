ee28142f317dd4671f36bb2109a7ee5de81ea620
karl2365 <karlbeksa@gmail.com>
2019-08-15 19:01:32 -0400

    :mute: removed console.logs

--- recipe/recipe-model.js
+++ recipe/recipe-model.js
@@ -14,21 +14,15 @@ function getRecipes() {
 }
 
 function getShoppingList(id) {
-    console.log(id);
     return db('ingredient as i')
         .innerJoin('rec_ing as ri', 'i.id', 'ri.ing_id')
         .select('i.name', 'i.qty')
         .where('ri.rec_id', '=', id);
-        // .andWhere('ri.ing_id', '=', 'i.id'); 
-        
-      
 }
 
 function getInstructions(id) {
-    console.log(id);
     return db('instruction as i')
     .innerJoin('rec_inst as ri', 'i.id', 'ri.inst_id')
     .select('i.name')
     .where('ri.rec_id', '=', id);
-    // .andWhere('ri.inst_id', '=', 'i.id'); 
 }
